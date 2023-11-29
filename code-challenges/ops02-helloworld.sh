ad748d19787992990fccfb314cbaff8a6cc11443
Krystal Reid <147119686+thechaoskrys@users.noreply.github.com>
2023-11-29 15:59:35 -0500

    Create ops02-helloworld.sh

---
+++ code-challenges/ops02-helloworld.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Script Name:                  Hello World
+# Author:                       Krystal Reid
+# Date of latest revision:      10/25/2023
+# Purpose:                      To complete ops challenge class 02. Print "hello world"
+
+# Declaration of variables
+
+# Declaration of functions
+
+
+# Main
+chmod +x helloworld.sh
+echo "hello world"
+
+# End
